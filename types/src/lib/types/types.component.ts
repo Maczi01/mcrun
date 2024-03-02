49e429ec0aa70154b87bd93107642add996f81f8
maczi01 <mateusz.w.twardy@gmail.com>
2024-03-02 01:10:13 +0100

    chore: created types library

---
+++ types/src/lib/types/types.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mcrun-types',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './types.component.html',
+  styleUrl: './types.component.css',
+})
+export class TypesComponent {}
