b80deb728bf5336205dabb3f6fc6756bbd339db4
maczi01 <mateusz.w.twardy@gmail.com>
2024-03-11 10:13:28 +0100

    chore: added some header

--- apps/mcrun/src/app/app.component.ts
+++ apps/mcrun/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {Component} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {HeaderComponent} from "./components/header/header.component";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, HeaderComponent],
   selector: 'mcrun-root',
   templateUrl: './app.component.html',
 })
---
+++ apps/mcrun/src/app/components/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'mcrun-app-header', // Selektor, który używasz do umieszczania komponentu w szablonie HTML.
+  templateUrl: './header.component.html', // Ścieżka do pliku szablonu HTML dla tego komponentu.
+})
+export class HeaderComponent {
+  title = 'McRun';
+
+  constructor() {
+    // Konstruktor dla inicjalizacji komponentu.
+  }
+
+  // Możesz dodać metody, które będą obsługiwać różne zdarzenia, np. kliknięcie przycisku w nagłówku.
+  onMenuClick() {
+    console.log('Menu clicked');
+  }
+}
