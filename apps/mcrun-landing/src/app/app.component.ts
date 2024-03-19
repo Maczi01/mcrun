a14f5ddf1aa65064c421bb853d42efc3fa00a4b7
maczi01 <mateusz.w.twardy@gmail.com>
2024-03-19 21:11:39 +0100

    feat: some header work

--- apps/mcrun-landing/src/app/app.component.ts
+++ apps/mcrun-landing/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from "./header/header.component";
+import {HeaderComponent} from "./components/header/header.component";
 
 @Component({
   selector: 'mcrun-landing-root',
