733f6dc5bbdb837a519a9f00c5e05994fe383e3f
maczi01 <mateusz.w.twardy@gmail.com>
2024-03-21 00:07:54 +0100

    feat: show map

--- apps/mcrun-landing/src/app/components/map/mapbox-map.component.ts
+++ apps/mcrun-landing/src/app/components/map/mapbox-map.component.ts
@@ -9,15 +9,30 @@ import * as mapboxgl from 'mapbox-gl';
 })
 export class MapboxMapComponent implements OnInit {
 
-  constructor() { }
+  map: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/streets-v11';
+
+  lat = 26.3398;
+  lng = -81.7787;
+  constructor() {}
 
   ngOnInit() {
-    (mapboxgl as any).accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
-    var map = new mapboxgl.Map({
-      container: 'map', // container ID
-      style: 'mapbox://styles/mapbox/streets-v11', // style URL
-      center: [54.61, -5.896], // starting position [lng, lat]
-      zoom: 9 // starting zoom
+    mapboxgl as typeof mapboxgl;
+    this.map = new mapboxgl.Map({
+      accessToken:
+        'pk.eyJ1IjoiZHBpZXRyb2NhcmxvIiwiYSI6ImNram9tOGFuMTBvb3oyeXFsdW5uYmJjNGQifQ._zE6Mub0-Vpl7ggMj8xSUQ',
+      container: 'map',
+      style: this.style,
+      zoom: 2,
+      center: [this.lng, this.lat],
     });
   }
+  // ngOnInit() {
+  //   (mapboxgl as any).accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+  //   var map = new mapboxgl.Map({
+  //     container: 'map', // container ID
+  //     style: 'mapbox://styles/mapbox/streets-v11', // style URL
+  //     center: [54.61, -5.896], // starting position [lng, lat]
+  //     zoom: 9 // starting zoom
+  //   });
 }
