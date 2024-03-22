af727f1f78f8e9b9bfb6ce3cdae87fc637eede8c
maczi01 <mateusz.w.twardy@gmail.com>
2024-03-22 14:29:56 +0100

    feat: mapbox component, removed unused code

--- apps/mcrun-landing/src/app/components/map/mapbox-map.component.ts
+++ apps/mcrun-landing/src/app/components/map/mapbox-map.component.ts
@@ -26,12 +26,4 @@ export class MapboxMapComponent implements OnInit {
       center: [this.lng, this.lat],
     });
   }
-  // ngOnInit() {
-  //   (mapboxgl as any).accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
-  //   var map = new mapboxgl.Map({
-  //     container: 'map', // container ID
-  //     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  //     center: [54.61, -5.896], // starting position [lng, lat]
-  //     zoom: 9 // starting zoom
-  //   });
 }
