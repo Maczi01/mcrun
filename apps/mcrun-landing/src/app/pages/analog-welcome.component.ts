d2fcf293e3447853905e2c710d22a3b084083060
maczi01 <mateusz.w.twardy@gmail.com>
2024-03-23 23:27:01 +0100

    feat: modified map and main view

--- apps/mcrun-landing/src/app/pages/analog-welcome.component.ts
+++ apps/mcrun-landing/src/app/pages/analog-welcome.component.ts
@@ -1,14 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {HeaderComponent} from "../components/header/header.component";
 import {MapboxMapComponent} from "../components/map/mapbox-map.component";
 
 @Component({
   selector: 'mcrun-landing-analog-welcome',
   standalone: true,
-  // host: {
-  //   class:
-  //     'flex min-h-screen flex-col text-zinc-900 bg-zinc-50 px-4 pt-8 pb-32',
-  // },
   templateUrl: './analog-welcome.component.html',
   imports: [
     HeaderComponent,
@@ -16,8 +12,4 @@ import {MapboxMapComponent} from "../components/map/mapbox-map.component";
   ]
 })
 export class AnalogWelcomeComponent {
-  public count = 0;
-  public increment() {
-    this.count++;
-  }
 }
