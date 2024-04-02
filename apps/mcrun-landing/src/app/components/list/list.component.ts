e4e28fe36d8fffbe46553c967a09edffc1856bb3
maczi01 <mateusz.w.twardy@gmail.com>
2024-04-02 23:40:54 +0200

    chore: created list component

---
+++ apps/mcrun-landing/src/app/components/list/list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'list-component',
+  templateUrl: './list.component.html',
+})
+export class ListComponent {}
