e6b1e32d498e033cd996215c7e2c95bf17387e66
maczi01 <mateusz.w.twardy@gmail.com>
2024-04-05 19:03:21 +0200

    chore: populated data

--- apps/mcrun-landing/src/app/app.component.ts
+++ apps/mcrun-landing/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from "./components/header/header.component";
 
 @Component({
   selector: 'mcrun-landing-root',
--- apps/mcrun-landing/src/app/components/run-item/run-item.component.ts
+++ apps/mcrun-landing/src/app/components/run-item/run-item.component.ts
@@ -1,13 +1,15 @@
 import {Component, Input} from '@angular/core';
+import {DatePipe} from "@angular/common";
 export class Run {
   name: string;
   date: string;
 }
 @Component({
-  standalone: true,
-  selector: 'run-item-component',
-  templateUrl: './run-item.component.html',
+    standalone: true,
+    selector: 'run-item-component',
+    templateUrl: './run-item.component.html',
+    imports: [DatePipe],
 })
 export class RunItemComponent {
-  @Input() run: Run;
+    @Input() run: Run;
 }
