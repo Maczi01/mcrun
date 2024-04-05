7b2dca7f16f17c9b70490941dc0a45322339bd46
maczi01 <mateusz.w.twardy@gmail.com>
2024-04-05 09:23:36 +0200

    chore: refactor, removed unused components

--- apps/mcrun-landing/src/app/components/list/list.component.ts
+++ apps/mcrun-landing/src/app/components/list/list.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {RunItemComponent} from "../run-item/run-item.component";
 
 @Component({
   standalone: true,
   selector: 'list-component',
+  imports: [RunItemComponent],
   templateUrl: './list.component.html',
 })
 export class ListComponent {}
---
+++ apps/mcrun-landing/src/app/components/run-item/run-item.component.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from '@angular/core';
+export class Run {
+  name: string;
+}
+@Component({
+  standalone: true,
+  selector: 'run-item-component',
+  templateUrl: './run-item.component.html',
+})
+export class RunItemComponent {
+  @Input() run: Run;
+}
