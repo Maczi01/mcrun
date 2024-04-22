ff584e6244ab547bf4a5efb44ca9dadbc611eaa2
maczi01 <mateusz.w.twardy@gmail.com>
2024-04-22 19:54:55 +0200

    chore: subpage created

--- apps/mcrun-landing/src/app/app.config.ts
+++ apps/mcrun-landing/src/app/app.config.ts
@@ -2,9 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideFileRouter } from '@analogjs/router';
+import {withComponentInputBinding} from "@angular/router";
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideFileRouter(withComponentInputBinding()),
     provideFileRouter(),
     provideClientHydration(),
     provideHttpClient(withFetch()),
