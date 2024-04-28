c7020c9207330136af465912bd52d069daefe1fd
maczi01 <mateusz.w.twardy@gmail.com>
2024-04-28 22:01:35 +0200

    chore: updated folders in pages

--- apps/mcrun-landing/src/app/pages/about/index.page.ts
+++ apps/mcrun-landing/src/app/pages/about/index.page.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import {HeaderComponent} from "../components/header/header.component";
+import {HeaderComponent} from "../../components/header/header.component";
 
 @Component({
-    selector: 'mcrun-home',
+    selector: 'mcrun-landing-home',
     standalone: true,
-    templateUrl: './home.component.html',
+    templateUrl: './about.component.html',
     imports: [HeaderComponent],
 })
 export default class HomePageComponent {}
